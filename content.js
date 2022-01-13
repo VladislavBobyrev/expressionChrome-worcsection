63bcd339c1b2d6622bb74a2e7ecfc698e6dfcf20
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2022-01-13 15:10:47 +0300

    Add files via upload

---
+++ content.js
@@ -0,0 +1,18 @@
+const body = document.querySelector('body').style.background = '#5d636e'
+const main = document.querySelector('#m-main-wrapper').style.background = '#5d636e'
+const mainb = document.querySelector('.m-history-wrapper.js-history-wrapper').style.background = '#5d636e'
+const maina = document.querySelector('.m-history-header-wrapper.js-history-header-wrapper').style.background = '#5d636e'
+const maino = document.querySelector('.m-chats-block.js-chats-block').style.background = '#5d636e'
+const mainz = document.querySelector('.m-chats-header-wrapper').style.background = '#5d636e'
+const mains = document.querySelector('.m-info-header-wrapper.js-info-header-wrapper').style.background = '#5d636e'
+const maini = document.querySelector('.m-info-data-wrapper.js-info-data-wrapper').style.background = '#5d636e'
+const maink = document.querySelector('.m-info-content-wrapper.js-info-content-wrapper.scrollbar.scroll-content').style.background = '#5d636e'
+const maik = document.querySelector('.m-info-actions-wrapper.js-info-actions-wrapper').style.background = '#5d636e'
+const mainm = document.querySelector('.m-info-actions-wrapper').style.background = '#5d636e'
+const mainn = document.querySelector('.m-new-message-wrapper.js-new-message-wrapper').style.background = '#5d636e'
+const ma = document.querySelector('.m-info-fields-wrapper.js-info-fields-wrapper').style.background = '#5d636e'
+
+const m = document.querySelector('.m-history-wrapper')
+m.style.maxWidht = '100%'
+const block = document.querySelector('.m-main-wrapper .m-content-block')
+block.style.maxWidth = '100%'
